01d0a36c34d5c0b5ee363958ebcc908a9b6b0457
Alberto Benitez <albertobenitezbahena@gmail.com>
2021-11-27 10:21:39 -0500

    Reto cu17

---
+++ Emigrant.App/Emigrant.App.Frontend/Pages/Entidad/registro.cshtml.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Emigrant.App.Persistencia;
+using Emigrant.App.Dominio;
+namespace Emigrant.App.Frontend.Pages
+{
+    public class RegistroModel : PageModel
+    {
+
+        private static IRepositorioEntidad _repoEntidad = new RepositorioEntidad(new Persistencia.AppContext());
+        [BindProperty]
+
+        public Entidad entidad { get; set; }
+
+        public IActionResult OnGet(int? entidadId)
+        {
+
+            if (entidadId.HasValue)
+            {
+                entidad = _repoEntidad.GetEntidad(entidadId.Value);
+            }
+            else
+            {
+                entidad = new Entidad();
+            }
+
+
+            if (entidad == null)
+            {
+                return RedirectToPage("../Migrante/migrante");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                if (entidad.id > 0)
+                {
+
+                    _repoEntidad.UpdateEntidad(entidad);
+                }
+                else
+                {
+                    Console.WriteLine(entidad);
+                    _repoEntidad.AddEntidad(entidad);
+                }
+            }
+            return RedirectToPage("../migrante/migrante");
+        }
+    }
+}
